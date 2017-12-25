16f10f84031c7715372779e3376ed7b442c187a1
Daniel Fredriksen <dfredriksen@cyint.technology>
2017-12-25 20:57:10 +0000

    Fixed issue with multiple validators for a single field

--- saga.js
+++ saga.js
@@ -8,18 +8,23 @@ export function* validateFormData(action) {
   let clonedFormValues = action.formValues;
   let isValid = true; 
   action.formValues.get(action.id).keySeq().forEach((field, fieldIndex) => {
-    let validationMessage = '';
-    const validatorConfig = action.validation[fieldIndex] || [];
     if (action.fieldData.getIn(['data', field, 'widget'], 'text') !== 'divider') {
-      for (let configIndex = 0; configIndex < validatorConfig.length; configIndex += 1) {
-        const value = clonedFormValues.getIn([action.id, field, 'value'], '');
-        const isFieldValid = validators[validatorConfig[configIndex].validationType](value);
-        validationMessage += isFieldValid ? '' : `${validatorConfig[configIndex].invalidMessage}. `;
-        clonedFormValues = clonedFormValues.setIn([action.id, field], ImmutableMap({ value, isValid: isFieldValid, validationMessage }));
-        if (!isFieldValid) {
-          isValid = false;
-        }
-      }              
+      let validationMessage = '';
+      const propertyOrder = action.fieldData.getIn(['data',field,'propertyOrder'], 0);
+      if(propertyOrder > 0) {
+        const validatorConfig = action.validation[propertyOrder-1] || [];     
+        let areAllValidatorsValid = true; 
+        for (let configIndex = 0; configIndex < validatorConfig.length; configIndex += 1) {
+          const value = clonedFormValues.getIn([action.id, field, 'value'], '');
+          const isFieldValid = validators[validatorConfig[configIndex].validationType](value);
+          validationMessage += isFieldValid ? '' : `${validatorConfig[configIndex].invalidMessage}. `;
+          if (!isFieldValid) {
+            isValid = false;
+            areAllValidatorsValid = false
+          }
+          clonedFormValues = clonedFormValues.setIn([action.id, field], ImmutableMap({ value, isValid: areAllValidatorsValid, validationMessage }));
+        } 
+      }             
     }
   });  
 
--- validators.js
+++ validators.js
@@ -1,7 +1,10 @@
 const validators = {
   required: (value) => (typeof value !== 'undefined' && value !== null && value !== ''),
   email: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/.test(value),
-
+  integer: (value) => /^\-?(0|[1-9]\d*)$/.test(value),
+  decimal: (value) => !isNaN(parseFloat(value)) && isFinite(value),
+  nonegative: (value) => parseInt(value) > 0,
+  website: (value) => /^(http|https):\/\/[^ "]+$/.test(value),
 };
 
 export default validators;
